cb72c0a9722d5d6a3ac67e3d7c871b31059c4069
Grazia Stracquadanio <graziastracquadanio@users.noreply.github.com>
2019-02-05 15:29:43 +0100

    Create js-exercise.ts

---
+++ js-exercise.ts
@@ -0,0 +1,35 @@
+interface TreeNode {
+
+}
+
+const whatever = {
+   prop1: {
+       label: '',
+       values: [0, 1, 2, 3]
+   },
+   prop2: {
+       label: '',
+       values: [1, 3, 5, 7],
+   },
+   prop3: {
+       label: '',
+       values: [0, 2, -4, 6],
+   },
+   prop4: {
+       label: '',
+       values: [0, 0, 0, -1],
+       isLeaf: true,
+   },
+};
+
+// output the properties of the given object in a list
+// assigning the key to the 'label' property
+const objToList = (obj) => ();
+
+// Output the list of keys of the objects that have
+// at least an odd value in the values property
+const whoHasOddValues = (list: TreeNode[]) => {};
+
+// Output the list of keys of the objects that have
+// at least a negative value in the values property
+const whoIsNegative = (list: TreeNode[]) => {};
